683ab0bd3968b984533b76f435e5b005bad0d5fc
T0myyde <lpmcbagi@gmail.com>
2022-01-03 11:07:51 +0100

    init

---
+++ src/main/java/ch/tom/furnaceupgrader/Furnaceupgrader.java
@@ -0,0 +1,17 @@
+package ch.tom.furnaceupgrader;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Furnaceupgrader extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
